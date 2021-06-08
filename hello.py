8655e8bf1c8e1f72194f8ce5611f8b110a4cb96a
Raphael Onofre <raphael.onofre@gmail.com>
2021-06-07 23:26:39 -0300

    Add relationship and database operation in index view.

--- hello.py
+++ hello.py
@@ -26,7 +26,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return f'<Role ${self.name}>'
@@ -51,13 +51,20 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session['name']
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    session['name'] = 'Stranger'
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.route('/user/<name>')
