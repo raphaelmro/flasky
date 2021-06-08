231510f3c132e1b58f3bf20d70d5c97caa64e3d2
Raphael Onofre <raphael.onofre@gmail.com>
2021-06-08 01:09:54 -0300

    Fix table name

--- hello.py
+++ hello.py
@@ -47,7 +47,7 @@ class Role(db.Model):
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
